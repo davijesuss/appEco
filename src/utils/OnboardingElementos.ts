3a1af0a1092e12aabaac58fc03c93e8b128514e6
Davi Jesus <davi7195@gmail.com>
2024-07-02 10:19:29 -0300

    barraProgressoIcone

--- src/utils/OnboardingElementos.ts
+++ src/utils/OnboardingElementos.ts
@@ -28,14 +28,14 @@ const secoes = [
     titulo: 'Categoria dos Forms',
     imagem: CirculoCategoria,
     categorias: [
-      { nome: 'Lorem ipsum', acao: () => console.log('Ação 1'), style: { backgroundColor: 'rgba(12, 124, 186, 1)', color: 'white' } },
-      { nome: 'Lorem ipsum', acao: () => console.log('Ação 2'), style: { backgroundColor: 'rgba(108, 136, 0, 1)', color: 'white' } },
-      { nome: 'Lorem ipsum', acao: () => console.log('Ação 3'), style: { backgroundColor: 'rgba(201, 45, 57, 1)', color: 'white' } },
-      { nome: 'Lorem ipsum', acao: () => console.log('Ação 4'), style: { backgroundColor: 'rgba(151, 87, 19, 1)', color: 'white' } },
-      { nome: 'Lorem ipsum', acao: () => console.log('Ação 5'), style: { backgroundColor: 'rgba(252, 196, 56, 1)', color: 'white' } },
-      { nome: 'Lorem ipsum', acao: () => console.log('Ação 6'), style: { backgroundColor: 'rgba(239, 141, 34, 1)', color: 'white' } },
-      { nome: 'Lorem ipsum', acao: () => console.log('Ação 7'), style: { backgroundColor: 'rgba(94, 94, 94, 1)', color: 'white' } },
-      { nome: 'Lorem ipsum', acao: () => console.log('Ação 8'), style: { backgroundColor: 'rgba(75, 90, 194, 1)', color: 'white' } },
+      { nome: 'Gestão Sustentável Águas', acao: () => console.log('Ação 1'), style: { backgroundColor: 'rgba(12, 124, 186, 1)', color: 'white' } },
+      { nome: 'Eficiência Energética', acao: () => console.log('Ação 2'), style: { backgroundColor: 'rgba(108, 136, 0, 1)', color: 'white' } },
+      { nome: 'Projeto Sustentável', acao: () => console.log('Ação 3'), style: { backgroundColor: 'rgba(201, 45, 57, 1)', color: 'white' } },
+      { nome: 'Qualidade Urbana', acao: () => console.log('Ação 4'), style: { backgroundColor: 'rgba(151, 87, 19, 1)', color: 'white' } },
+      { nome: 'Verde Urbano', acao: () => console.log('Ação 5'), style: { backgroundColor: 'rgba(252, 196, 56, 1)', color: 'white' } },
+      { nome: 'Contribuições Ambientais', acao: () => console.log('Ação 6'), style: { backgroundColor: 'rgba(239, 141, 34, 1)', color: 'white' } },
+      { nome: 'Gestão de Resíduos', acao: () => console.log('Ação 7'), style: { backgroundColor: 'rgba(94, 94, 94, 1)', color: 'white' } },
+      { nome: 'Bonificações', acao: () => console.log('Ação 8'), style: { backgroundColor: 'rgba(75, 90, 194, 1)', color: 'white' } },
     ]
   },
   {
